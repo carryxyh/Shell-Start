02ca4168cbd0afd7bc6c02ae257dfdeeef29bebf
ziyuan <ziyuan@2dfire.com>
2017-02-28 17:01:42 +0800

    检查nginx状态的脚本
    nginx server默认本机，输出位置默认/tmp/nginxlog.log

---
+++ CheckNginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NginxServer='http://127.0.0.1'
+Check_Nginx_Server()
+{
+
+    http_status_code=$(curl -m 5 -s -i  -w %{http_code} -o /tmp/nginxlog.log $NginxServer)
+    if [ $http_status_code -eq 000 -o $http_status_code -ge 500 ];then
+            echo "check http server error \nhttp_status_code is"  $http_status_code > /tmp/nginxlog.log
+    else
+            http_content=$(curl -s ${NginxServer}) 
+            echo "service status ok\n"$http_content
+    fi
+}
+
+Check_Nginx_Server
